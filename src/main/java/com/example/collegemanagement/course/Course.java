f4e3b6327b0c700a5f47740427b417e9c92eaf20
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-28 05:43:14 -0400

    Fix courses

--- src/main/java/com/example/collegemanagement/course/Course.java
+++ src/main/java/com/example/collegemanagement/course/Course.java
@@ -6,13 +6,14 @@ import com.example.collegemanagement.student.Student;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name="course")
 public class Course {
-@Id
-@GeneratedValue(strategy = GenerationType.SEQUENCE)
-private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
     private String courseCode;
     private String name;
     private String description;
@@ -24,6 +25,19 @@ private Long id;
     @JoinColumn(name = "department_id")
     private Department department;
     private int maxEnrollment;
+
+    public Course() {
+    }
+
+    public Course(String courseCode, String name, String description, int credits, Professor instructor, Department department, int maxEnrollment) {
+        this.courseCode = courseCode;
+        this.name = name;
+        this.description = description;
+        this.credits = credits;
+        this.instructor = instructor;
+        this.department = department;
+        this.maxEnrollment = maxEnrollment;
+    }
 
     public Department getDepartment() {
         return department;
@@ -39,5 +53,32 @@ private Long id;
 
     public void setInstructor(Professor instructor) {
         this.instructor = instructor;
+    }
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseCode='" + courseCode + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", credits=" + credits +
+                ", instructor=" + instructor +
+                ", department=" + department +
+                ", maxEnrollment=" + maxEnrollment +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return credits == course.credits && maxEnrollment == course.maxEnrollment && Objects.equals(id, course.id) && Objects.equals(courseCode, course.courseCode) && Objects.equals(name, course.name) && Objects.equals(description, course.description) && Objects.equals(instructor, course.instructor) && Objects.equals(department, course.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseCode, name, description, credits, instructor, department, maxEnrollment);
     }
 }

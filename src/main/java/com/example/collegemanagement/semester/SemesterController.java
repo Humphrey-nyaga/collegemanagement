5454d555fb080c986292938ce2bd5ff3fd614bad
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-28 06:04:40 -0400

    Create rest controllers

---
+++ src/main/java/com/example/collegemanagement/semester/SemesterController.java
@@ -0,0 +1,9 @@
+package com.example.collegemanagement.semester;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/semester")
+public class SemesterController {
+}
